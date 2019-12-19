cb43933daefcc3313f88d9ef4624c5d494bf783a
Long Trac <let020@my.utsa.edu>
2019-12-18 21:04:50 -0600

    Added Detail View

---
+++ RestaurantOrderManager/RestaurantOrderManager/Views/Home/OrderDetail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RestaurantOrderManager.Views.Home
+{
+    public class OrderDetail : PageModel
+    {
+        public void OnGet()
+        {
+            //sad hihi
+        }
+    }
+}
